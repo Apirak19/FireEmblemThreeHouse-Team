d4f3edbc3f25625d91e678bb6dc1dc82c8987ae7
Apirak <apirakfakin@gmail.com>
2023-12-19 16:16:44 +0700

    build init

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-"use client"
-import React, {useState} from "react";
-import data from './data.json'
+"use client";
+import React from "react";
+
+import characterData from "./data.json";
 
 export default function page() {
-  const [characterData, setCharacterData] = useState([])
-  const fetchData = async () => {
-    const response = await fetch("http://localhost3000")
-    const data = await response.json()
-}
-
   return (
     <main className="min-h-screen">
       <h1 className="text-5xl font-bold text-center p-3 pt-5">
@@ -18,9 +13,31 @@ export default function page() {
         Class and Ability Guide
       </h2>
       <div>
-        {Object(data).map(() => (
-          <div>
-            
+        {characterData.map((character) => (
+          <div key={character.name} className="m-5">
+            <h1 className="text-2xl font-semibold">{character.name}</h1>
+            <div className="p-3 pb-0">
+              <span className="font-semibold">Type: </span>
+              <span className="inline-block">{ Array.isArray(character.type)? character.type.join(", ") : character.type}</span>
+            </div>
+            <div className="px-3">
+              <span className="font-semibold">Class: </span>
+              <span className="inline-block">
+                {character.firstClass}
+                {" => "}
+                {character.intermediateClasses}
+                {" => "}
+                {character.advancedClasses}
+                {" => "}
+                {character.masterClasses}
+              </span>
+            </div>
+            <div className="px-3">
+              <span className="font-semibold">Abilities: </span>
+              <span>
+                {character.abilities.join(", ")}
+              </span>
+            </div>
           </div>
         ))}
       </div>
