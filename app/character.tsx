ecc260ee3195eebd96cf5ca913aa6cb0c22cceb9
Apirak <apirakfakin@gmail.com>
2023-12-25 11:51:49 +0700

    background adjusted

--- app/character.tsx
+++ app/character.tsx
@@ -1,9 +1,94 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import characterData from "./data.json";
 
-export default function character() {
-  return (
-    <div>
-      
-    </div>
-  )
+const Character = [...characterData].sort((a, b) =>
+  a.name.localeCompare(b.name)
+);
+export default function CharacterSection() {
+   return (
+      <div id="CharacterSection">
+         {Character.map((character, index) => (
+            <div
+               key={character.name}
+               id={character.name}
+               className="my-10 mx-10 bg-yellow-100 p-5 rounded-3xl w-[750px] flex flex-col justify-center"
+            >
+               <h1 className="text-2xl font-light text-slate-900 my-1">
+                  {character.name}
+               </h1>
+               {/* -------------------------- Line -------------------------- */}
+               <div className="w-full bg-slate-900 h-[1px] blur-[1px]mb-[2px]"></div>
+               <div className="w-full bg-yellow-900 h-[1px] blur-[2px] mb-3"></div>
+
+               <div className="flex">
+                  <img
+                     src={character.image}
+                     alt={character.name}
+                     title={character.name}
+                     className="rounded-full w-[150px] h-[150px]"
+                  />
+               <div className="data-section">
+                  <div className="p-3 pb-0">
+                     <p className="font-semibold w-[125px] inline-block">Type: </p>
+                     <span className="inline-block">
+                        {Array.isArray(character.type)
+                           ? character.type.join(", ")
+                           : character.type}
+                     </span>
+                  </div>
+                  <div className="px-3">
+                     <p className="font-semibold w-[125px] inline-block">Class: </p>
+                     <p className="inline-block">
+                        <span className="text-slate-300">{character.firstClass}</span>
+                        {" => "}
+                        <span className="text-green-500">
+                           {character.beginnerClasses}
+                        </span>
+                        {" => "}
+                        <span className="text-blue-500">
+                           {character.intermediateClasses}
+                        </span>
+                        {" => "}
+                        <span className="text-purple-500">
+                           {character.advancedClasses}
+                        </span>
+                        {" => "}
+                        <span className="text-yellow-600">{character.masterClasses}</span>
+                     </p>
+                  </div>
+                  {character.classAbilities && (
+                     <div className="px-3">
+                        <p className="font-semibold w-[125px] inline-block">
+                           Class Abilities:
+                        </p>
+                        <p className="inline-block">
+                           <span className="text-red-500 ">
+                              {character.classAbilities.join(", ")}
+                           </span>
+                        </p>
+                     </div>
+                  )}
+                  <p className="px-3 w-[600px]">
+                     <span className="font-semibold w-[125px] inline-block">
+                        Abilities:
+                     </span>
+                     <span className="inline-block">
+                        {character.abilities.join(", ")}
+                     </span>
+                  </p>
+                  <div className="px-3">
+                     <p className="font-semibold w-[125px] inline-block">
+                        Requirements:
+                     </p>
+                     <span className="text-red-500 inline-block">
+                        {character.requirements.join(", ")}
+                     </span>
+                  </div>
+               </div>
+               </div>
+            </div>
+         ))}
+      </div>
+   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { Link } from "react-scroll";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import CharacterSection from "./character";
 
 import characterData from "./data.json";
 
@@ -12,21 +13,23 @@ export default function page() {
 
   const characterList = Character.map((item) => (
     <Link
-      className="cursor-pointer"
+      className="m-1 cursor-pointer hover:text-yellow-500 hover:underline"
       key={item.name}
       to={item.name}
       smooth={true}
       duration={200}
     >
-      {" "}
       {item.name}
     </Link>
   ));
   return (
-    <main className="min-h-screen flex flex-col items-center overflow-hidden box-border">
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <div className="w-[750px] bg-yellow-100 flex flex-col">
+      
       <Link
         className="fixed bottom-5 right-5"
-        to="top"
+        onClick={() => {window.scrollTo({top: 0, behavior: "smooth"})}}
+        to=""
         smooth={true}
         duration={200}
       >
@@ -42,93 +45,13 @@ export default function page() {
       <h2 className="text-xl font-bold text-center mt-5">
         Class and Ability Guide
       </h2>
-      <div className="mt-10 px-10">{characterList}</div>
+      <div className="w-[750px] bg-yellow-100 flex flex-wrap mt-5">{characterList}</div>
+      </div>
 
       {/* -------------------------- Character -------------------------- */}
-      <div>
-        {Character.map((character, index) => (
-          <div
-            key={character.name}
-            id={character.name}
-            className="my-10 mx-10 bg-slate-700 p-10 rounded-3xl w-[750px] flex flex-col justify-center"
-          >
-            <h1 className="text-2xl font-semibold text-pink-200 my-1">
-              {character.name}
-            </h1>
-            {/* -------------------------- Line -------------------------- */}
-            <div className="w-full bg-slate-300 h-1 blur-[1.5px] mb-3"></div>
-
-            <div className="flex">
-              <img
-                src={character.image}
-                alt={character.name}
-                title={character.name}
-                className="rounded-full w-[150px]"
-              />
-            </div>
-            <div className="data-section">
-              <div className="p-3 pb-0">
-                <p className="font-semibold w-[125px] inline-block">Type: </p>
-                <span className="inline-block">
-                  {Array.isArray(character.type)
-                    ? character.type.join(", ")
-                    : character.type}
-                </span>
-              </div>
-              <div className="px-3">
-                <p className="font-semibold w-[125px] inline-block">Class: </p>
-                <p className="inline-block">
-                  <span className="text-slate-300">{character.firstClass}</span>
-                  {" => "}
-                  <span className="text-green-500">
-                    {character.beginnerClasses}
-                  </span>
-                  {" => "}
-                  <span className="text-blue-500">
-                    {character.intermediateClasses}
-                  </span>
-                  {" => "}
-                  <span className="text-purple-500">
-                    {character.advancedClasses}
-                  </span>
-                  {" => "}
-                  <span className="text-yellow-600">
-                    {character.masterClasses}
-                  </span>
-                </p>
-              </div>
-              {character.classAbilities && (
-                <div className="px-3">
-                  <p className="font-semibold w-[125px] inline-block">
-                    Class Abilities:
-                  </p>
-                  <p className="inline-block">
-                    <span className="text-red-500 ">
-                      {character.classAbilities.join(", ")}
-                    </span>
-                  </p>
-                </div>
-              )}
-              <p className="px-3 w-[600px]">
-                <span className="font-semibold w-[125px] inline-block">
-                  Abilities:
-                </span>
-                <span className="inline-block">
-                  {character.abilities.join(", ")}
-                </span>
-              </p>
-              <div className="px-3">
-                <p className="font-semibold w-[125px] inline-block">
-                  Requirements:
-                </p>
-                <span className="text-red-500 inline-block">
-                  {character.requirements.join(", ")}
-                </span>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      
+      <CharacterSection/>
+      
     </main>
   );
 }
