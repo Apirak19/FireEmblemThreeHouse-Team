f965054c23870ed1ae8ec6f7cd7b49093137cb5d
Apirak <apirakfakin@gmail.com>
2023-12-25 15:18:49 +0700

    before del map

--- app/character.tsx
+++ app/character.tsx
@@ -6,89 +6,101 @@ const Character = [...characterData].sort((a, b) =>
   a.name.localeCompare(b.name)
 );
 export default function CharacterSection() {
-   return (
-      <div id="CharacterSection">
-         {Character.map((character, index) => (
-            <div
-               key={character.name}
-               id={character.name}
-               className="my-10 mx-10 bg-yellow-100 p-5 rounded-3xl w-[750px] flex flex-col justify-center"
-            >
-               <h1 className="text-2xl font-light text-slate-900 my-1">
-                  {character.name}
-               </h1>
-               {/* -------------------------- Line -------------------------- */}
-               <div className="w-full bg-slate-900 h-[1px] blur-[1px]mb-[2px]"></div>
-               <div className="w-full bg-yellow-900 h-[1px] blur-[2px] mb-3"></div>
-
-               <div className="flex">
-                  <img
-                     src={character.image}
-                     alt={character.name}
-                     title={character.name}
-                     className="rounded-full w-[150px] h-[150px]"
-                  />
-               <div className="data-section">
-                  <div className="p-3 pb-0">
-                     <p className="font-semibold w-[125px] inline-block">Type: </p>
-                     <span className="inline-block">
-                        {Array.isArray(character.type)
-                           ? character.type.join(", ")
-                           : character.type}
-                     </span>
-                  </div>
-                  <div className="px-3">
-                     <p className="font-semibold w-[125px] inline-block">Class: </p>
-                     <p className="inline-block">
-                        <span className="text-slate-300">{character.firstClass}</span>
-                        {" => "}
-                        <span className="text-green-500">
-                           {character.beginnerClasses}
-                        </span>
-                        {" => "}
-                        <span className="text-blue-500">
-                           {character.intermediateClasses}
-                        </span>
-                        {" => "}
-                        <span className="text-purple-500">
-                           {character.advancedClasses}
-                        </span>
-                        {" => "}
-                        <span className="text-yellow-600">{character.masterClasses}</span>
-                     </p>
-                  </div>
-                  {character.classAbilities && (
-                     <div className="px-3">
-                        <p className="font-semibold w-[125px] inline-block">
-                           Class Abilities:
-                        </p>
-                        <p className="inline-block">
-                           <span className="text-red-500 ">
-                              {character.classAbilities.join(", ")}
-                           </span>
-                        </p>
-                     </div>
-                  )}
-                  <p className="px-3 w-[600px]">
-                     <span className="font-semibold w-[125px] inline-block">
-                        Abilities:
-                     </span>
-                     <span className="inline-block">
-                        {character.abilities.join(", ")}
-                     </span>
+  return (
+    <div id="CharacterSection">
+      {Character.map((character, index) => (
+        <section
+          key={character.name}
+          id={character.name}
+          className="my-10 mx-10 bg-yellow-100 p-5 rounded-3xl w-[750px] flex flex-col justify-center"
+        >
+          <h1 className="text-2xl font-light text-slate-900 my-1">
+            {character.name}
+          </h1>
+          {/* -------------------------- Line -------------------------- */}
+          <div className="w-full bg-slate-900 h-[1px] blur-[1px]mb-[2px]"></div>
+          <div className="w-full bg-yellow-900 h-[1px] blur-[2px] mb-3"></div>
+          {/* -------------------------- Detail -------------------------- */}
+          <div className="flex">
+            <img
+              src={character.image}
+              alt={character.name}
+              title={character.name}
+              className="rounded-full w-[150px] h-[150px]"
+            />
+            {/* -------------------------- Text -------------------------- */}
+            <div className="data-section">
+              {/* -------------------------- Type -------------------------- */}
+              <article className="flex item-start max-w-[550px]">
+                <p className="pl-5 font-semibold w-[150px] inline-block">
+                  Type:{" "}
+                </p>
+                <span className="inline-block">
+                  {Array.isArray(character.type)
+                    ? character.type.join(", ")
+                    : character.type}
+                </span>
+              </article>
+              {/* -------------------------- Class -------------------------- */}
+              <article className="flex item-start max-w-[550px]">
+                <p className="pl-5 font-semibold w-[150px] inline-block">
+                  Class:{" "}
+                </p>
+                <p className="flex flex-col">
+                  <span className="text-slate-500">{character.firstClass}</span>
+                  <span className="text-green-500">
+                    {character.beginnerClasses}
+                  </span>
+                  <span className="text-blue-500">
+                    {character.intermediateClasses}
+                  </span>
+                  <span className="text-purple-500">
+                    {character.advancedClasses}
+                  </span>
+                  <span className="text-yellow-600">
+                    {character.masterClasses}
+                  </span>
+                </p>
+              </article>
+              {/* -------------------------- Class Abilities -------------------------- */}
+              {/* {character.classAbilities && (
+                <div className="px-3">
+                  <p className="pl-5 font-semibold w-[150px] inline-block">
+                    Class Abilities:
                   </p>
-                  <div className="px-3">
-                     <p className="font-semibold w-[125px] inline-block">
-                        Requirements:
-                     </p>
-                     <span className="text-red-500 inline-block">
-                        {character.requirements.join(", ")}
-                     </span>
-                  </div>
-               </div>
-               </div>
+                  <p className="inline-block">
+                    <span className="text-red-500 ">
+                      {character.classAbilities.join(", ")}
+                    </span>
+                  </p>
+                </div>
+              )} */}
+              {/* -------------------------- Abilities -------------------------- */}
+              <article className="flex item-start max-w-[550px]">
+                <p className="pl-5 font-semibold w-[150px] inline-block">
+                  Abilities:
+                </p>
+                <div className="flex flex-col">
+                  {character.abilities.map((ability) => (
+                    <span>{ability}</span>
+                  ))}
+                </div>
+              </article>
+              {/* -------------------------- Requirements -------------------------- */}
+              <article className="flex item-start max-w-[550px]">
+                <p className="pl-5 font-semibold w-[150px] inline-block">
+                  Requirements:
+                </p>
+                <div className="flex flex-col">
+                  {character.requirements.map((req) => (
+                    <span className="text-red-500 inline-block">{req}</span>
+                  ))}
+                </div>
+              </article>
             </div>
-         ))}
-      </div>
-   );
+          </div>
+        </section>
+      ))}
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,51 +7,66 @@ import CharacterSection from "./character";
 import characterData from "./data.json";
 
 export default function page() {
+  const [selectedCharacter, setSelectedCharacter] = useState("");
   const Character = [...characterData].sort((a, b) =>
     a.name.localeCompare(b.name)
   );
 
   const characterList = Character.map((item) => (
     <Link
-      className="m-1 cursor-pointer hover:text-yellow-500 hover:underline"
+      className="mx-1 cursor-pointer hover:text-yellow-500 hover:underline"
       key={item.name}
       to={item.name}
       smooth={true}
       duration={200}
+      onClick={() => {
+        setSelectedCharacter(item.name);
+      }}
     >
       {item.name}
     </Link>
   ));
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
-      <div className="w-[750px] bg-yellow-100 flex flex-col">
-      
-      <Link
-        className="fixed bottom-5 right-5"
-        onClick={() => {window.scrollTo({top: 0, behavior: "smooth"})}}
-        to=""
-        smooth={true}
-        duration={200}
-      >
-        <button className="bg-slate-400 rounded-full w-14 h-14">
-          <KeyboardArrowUpIcon fontSize="large" sx={{ color: "white" }} />
-        </button>
-      </Link>
+      <div className="w-[75vw] min-h-screen fixed top-0 bg-slate-800 bg-opacity-75 blur-sm -z-50"></div>
+      <div className="flex flex-col">
+        <Link
+          className="fixed bottom-5 right-5"
+          onClick={() => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+          to=""
+          smooth={true}
+          duration={200}
+        >
+          <button className="bg-slate-400 rounded-full w-14 h-14">
+            <KeyboardArrowUpIcon fontSize="large" sx={{ color: "white" }} />
+          </button>
+        </Link>
 
-      {/* -------------------------- Header -------------------------- */}
-      <h1 className="text-5xl font-bold text-center mt-5" id="top">
-        Fire Emblem Three Houses
-      </h1>
-      <h2 className="text-xl font-bold text-center mt-5">
-        Class and Ability Guide
-      </h2>
-      <div className="w-[750px] bg-yellow-100 flex flex-wrap mt-5">{characterList}</div>
+        {/* -------------------------- Header -------------------------- */}
+        <h1
+          className="text-5xl font-bold text-center mt-10 text-white"
+          id="top"
+        >
+          Fire Emblem Three Houses
+        </h1>
+        <h2 className="text-xl text-white font-bold text-center mt-5">
+          Class and Ability Guide for Each Character
+        </h2>
+        <h2 className="text-xl underline text-white font-bold text-start mt-5">
+          List of Characters
+        </h2>
+        <div className="w-[750px] flex flex-wrap mt-2 text-white">
+          {characterList}
+        </div>
+        <h1 className="text-yellow-500 font-bold text-center mt-5">
+          **The details are based on characters' ideal end-game status**
+        </h1>
       </div>
-
       {/* -------------------------- Character -------------------------- */}
-      
-      <CharacterSection/>
-      
+
+      <CharacterSection />
     </main>
   );
 }
