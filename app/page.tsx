5d0cfecc18bad955057545148c93e43acf03ad74
Apirak <apirakfakin@gmail.com>
2023-12-28 15:58:28 +0700

    fixing aside

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Link } from "react-scroll";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import CharacterSection from "./character";
-import './globals.css'
+import "./globals.css";
 
 import characterData from "./data.json";
 
@@ -35,9 +35,9 @@ export default function page() {
     </div>
   ));
   return (
-    <main className="min-h-screen max-w-[1280px] mx-auto">
+    <main>
       {/* ------------- To Top ------------- */}
-      <Link
+      {/* <Link
         className="fixed bottom-5 right-5"
         onClick={() => {
           window.scrollTo({ top: 0, behavior: "smooth" });
@@ -49,25 +49,21 @@ export default function page() {
         <button className="bg-slate-400 rounded-full w-[5vw] h-[5vw]">
           <KeyboardArrowUpIcon fontSize="large" sx={{ color: "white" }} />
         </button>
-      </Link>
+      </Link> */}
       {/* -------------------------- Sidebar -------------------------- */}
 
-      <aside className="relative top-0 left-0 inline h-screen">
-        <div
-          className="w-[300px] list flex flex-col justify-start items-center bg-slate-700 text-white font-light rounded-md shadow-2xl"
-          style={{
-            boxShadow:
-              "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
-          }}
-        >
+      <aside>
+        <div className="list">
           <h2 className="w-[300px] text-xl text-slate-700 font-bold mb-1 bg-slate-400 h-[100px] text-center">
             Characters
           </h2>
-          <div className="h-max-[cal(100%-300px)] overflow-y-auto">{characterList}</div>
+        </div>
+        <div className="h-[calc(100%-100px)] w-[300px] list flex flex-col justify-start items-center bg-slate-700 text-white font-light shadow-2xl overflow-y-auto overflow-x-hidden">
+          {characterList}
         </div>
       </aside>
       {/* -------------------------- Header -------------------------- */}
-      <section className="max-w-[calc(100%-200px)] relative left-[150px] flex flex-col items-center justify-start -z-10">
+      <section className="max-w-[calc(100%-200px)] relative left-[150px] flex flex-col items-center justify-start -z-10 col-span-2">
         <div className="w-[60vw] min-h-screen fixed top-0 bg-slate-800 bg-opacity-75 blur-xl -z-50">
           {/* ------------- background ------------- */}
         </div>
